a1bf13d8c6f0d2d144f3a182a84a6504e97be078
Samidy <samidy@proton.me>
2026-06-20 17:34:41 +0300

    AURA CHAT. AURA.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,12 @@ const config: CapacitorConfig = {
     allowNavigation: ['*.monochrome.tf'],
     errorPath: 'index.html',
   },
+  assets: {
+    iconBackgroundColor: '#000000',
+    iconBackgroundColorDark: '#000000',
+    splashBackgroundColor: '#000000',
+    splashBackgroundColorDark: '#000000',
+  },
 };
 
 export default config;
